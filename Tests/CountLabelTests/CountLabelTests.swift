7d07a1128fa7f22ad1833fdb7e9a0e5d2bb05351
marijnschilling <marijnschilling@gmail.com>
2017-06-22 14:11:46 +0200

    Add more and better tests

--- Tests/CountLabelTests/CountLabelTests.swift
+++ Tests/CountLabelTests/CountLabelTests.swift
@@ -8,7 +8,33 @@ class CountLabelTests: XCTestCase {
         
         let countLabel = CountLabel()
         countLabel.count(from: 0, to: 1, withDuration: 0.0) {
-            XCTAssertTrue(countLabel.text == "1.0")
+            XCTAssertEqual(countLabel.text ?? "", "1.0")
+            expect.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 5)
+    }
+
+    func testPrefix() {
+        let expect = self.expectation(description: "fetch overview")
+
+        let countLabel = CountLabel()
+        countLabel.prefix = "At the next beep it will be: "
+        countLabel.count(from: 0, to: 1, withDuration: 0.0) {
+            XCTAssertEqual(countLabel.text ?? "", "At the next beep it will be: 1.0")
+            expect.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 5)
+    }
+
+    func testPostfix() {
+        let expect = self.expectation(description: "fetch overview")
+
+        let countLabel = CountLabel()
+        countLabel.postfix = " seconds before universe implodes"
+        countLabel.count(from: 0, to: 1, withDuration: 0.0) {
+            XCTAssertEqual(countLabel.text ?? "", "1.0 seconds before universe implodes")
             expect.fulfill()
         }
 
