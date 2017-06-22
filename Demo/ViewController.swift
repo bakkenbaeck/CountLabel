b4c9ff37dcb48df3e727d88ac65c3822383c7778
marijnschilling <marijnschilling@gmail.com>
2017-06-22 11:47:06 +0200

    Add a demo project

---
+++ Demo/ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+import CountLabel
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let countLabel = CountLabel(frame: CGRect(x: (self.view.bounds.width * 0.5) - 22, y: (self.view.bounds.height * 0.5) - 50, width: 100, height: 44))
+        self.view.addSubview(CountLabel)
+
+        countLabel.count(from: 0, to: 100)
+    }
+}
