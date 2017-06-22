ceeb8e2d49485a172c891fd654b7e6b446821842
marijnschilling <marijnschilling@gmail.com>
2017-06-22 11:47:00 +0200

    Add the countlabel

--- Sources/CountLabel.swift
+++ Sources/CountLabel.swift
@@ -1,9 +1,63 @@
-//
-//  CountLabel.swift
-//  Bakken & Baeck
-//
-//  Created by Marijn Schilling on {TODAY}.
-//  Copyright © 2017 Bakken & Baeck. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class CountLabel: UILabel {
+    var animationDuration = 0.2
+
+    var currentValue: Int {
+        set {
+
+        }
+        get {
+            if self.progress >= self.totalTime {
+
+                return Int(self.endValue)
+            }
+
+            let percent = CGFloat(self.progress / self.totalTime)
+
+            return Int(self.startValue + (percent * (self.endValue - self.startValue)))
+        }
+    }
+
+    fileprivate var startValue: CGFloat = 0.0
+    fileprivate var endValue: CGFloat = 0.0
+    fileprivate var progress: TimeInterval = 0.0
+    fileprivate var lastUpdate: TimeInterval = 0.0
+    fileprivate var totalTime: TimeInterval = 0.0
+    fileprivate var easingRate: CGFloat = 0.0
+
+    fileprivate var timer: CADisplayLink?
+
+    func count(from startValue: CGFloat, to endValue: CGFloat, withDuration duration: Double = 2.0) {
+        self.startValue = startValue
+        self.endValue = endValue
+
+        self.timer?.invalidate()
+        self.timer = nil
+
+        self.easingRate = 3.0
+        self.progress = 0
+        self.totalTime = duration
+        self.lastUpdate = Date.timeIntervalSinceReferenceDate
+
+        let timer = CADisplayLink(target: self, selector: #selector(updateValue))
+        timer.preferredFramesPerSecond = 24
+        timer.add(to: .current, forMode: .defaultRunLoopMode)
+
+        self.timer = timer
+    }
+
+    func updateValue() {
+        let now = Date.timeIntervalSinceReferenceDate
+        self.progress = self.progress + (now - self.lastUpdate)
+        self.lastUpdate = now
+
+        if self.progress >= self.totalTime {
+            self.timer?.invalidate()
+            self.timer = nil
+            self.progress = self.totalTime
+        }
+
+        self.text = "\(self.currentValue)"
+    }
+}
