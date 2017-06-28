5da9d8ac860d78bc72225118b88e3ab3aebaa0f2
Igor Ranieri Elland <igor@elland.me>
2017-06-28 10:33:22 +0200

    Forgot to default it to nil.

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -9,26 +9,26 @@ class ViewController: UIViewController {
         let countLabel = CountLabel(frame: CGRect(x: (self.view.bounds.width * 0.5) - 22, y: 50, width: 100, height: 44))
         self.view.addSubview(countLabel)
 
-        countLabel.count(from: 0, to: 10) {}
+        countLabel.count(from: 0, to: 10)
 
         let countLabel2 = CountLabel(frame: CGRect(x: (self.view.bounds.width * 0.5) - 22, y: 150, width: 100, height: 44))
         self.view.addSubview(countLabel2)
 
-        countLabel2.count(from: 0, to: 100) {}
+        countLabel2.count(from: 0, to: 100)
 
         let countLabel3 = CountLabel(frame: CGRect(x: (self.view.bounds.width * 0.5) - 22, y: 250, width: 100, height: 44))
         self.view.addSubview(countLabel3)
 
-        countLabel3.count(from: 0, to: 1000) {}
+        countLabel3.count(from: 0, to: 1000)
 
         let countLabel4 = CountLabel(frame: CGRect(x: (self.view.bounds.width * 0.5) - 22, y: 350, width: 100, height: 44))
         self.view.addSubview(countLabel4)
 
-        countLabel4.count(from: 0, to: 10000) {}
+        countLabel4.count(from: 0, to: 10000)
 
         let countLabel5 = CountLabel(frame: CGRect(x: (self.view.bounds.width * 0.5) - 22, y: 450, width: 100, height: 44))
         self.view.addSubview(countLabel5)
 
-        countLabel5.count(from: 0, to: 100000) {}
+        countLabel5.count(from: 0, to: 100000)
     }
 }
--- Sources/CountLabel.swift
+++ Sources/CountLabel.swift
@@ -35,7 +35,7 @@ open class CountLabel: UILabel {
 
     private var completion: ((Void) -> Void)?
 
-    open func count(from startValue: Int, to endValue: Int, withDuration duration: Double = 2.0, completion: VoidCompletionBlock) {
+    open func count(from startValue: Int, to endValue: Int, withDuration duration: Double = 2.0, completion: VoidCompletionBlock = nil) {
         self.completion = completion
 
         self.startValue = startValue
